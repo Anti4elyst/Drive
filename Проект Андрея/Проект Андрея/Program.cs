5703e30128cc7f3cb804553c2989d05c1a444d65
Anti4elyst <55922678+Anti4elyst@users.noreply.github.com>
2019-10-15 18:23:09 +0300

    Add files via upload

---
+++ Проект Андрея/Проект Андрея/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проект_Андрея
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        treug odin = new treug();
+            odin.info();
+            bool l = true;
+            while (l == true)
+            {
+                odin.spros();
+                string resh = Console.ReadLine();
+                switch (resh)
+                {
+                    case "Площадь": odin.Plosh(); break;
+                    case "Углы": odin.ugly(); break;
+                    case "Новый": odin.kof(); break;
+
+                }
+            }
+            
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+     class treug
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int k;
+         
+
+
+        public void Plosh()
+        {
+            
+            int p = a + b + c;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine($"Площадь этого треугольника равна:{s}");
+
+        }
+        public void ugly()
+        {
+            double cos1 = (Math.Pow(b, 2) + Math.Pow(c, 2) - Math.Pow(a, 2))/2*b*c;
+            double cos2 = (Math.Pow(a, 2) + Math.Pow(c, 2) - Math.Pow(b, 2)) / 2 * a * c;
+            double cos3 = (Math.Pow(b, 2) + Math.Pow(a, 2) - Math.Pow(c, 2)) / 2 * b * a;
+            double ugol1 = Math.Acos(cos1);
+            double ugol2 = Math.Acos(cos2);
+            double ugol3 = Math.Acos(cos3);
+            Console.WriteLine($"Первый угол равен:{ugol1} Второй угол равен:{ugol2} Третий угол равен:{ugol3}");
+        }
+        public void kof()
+        {
+            Console.Write("Введите коэффициент:");
+             k = Convert.ToInt32( Console.ReadLine());
+            if ((k == 0)||(k==1))
+            { Console.WriteLine("Стороны треугольника не поменялись."); }
+            else
+            {
+                a *= k;
+                b *= k;
+                c *= k;
+                Console.Write($"Новые стороны треугольника равны:{a},{b},{c}");
+            }
+            
+        }
+        public void spros()
+        {
+            Console.WriteLine("Что вы хотите найти?");
+            Console.WriteLine("1.Площадь этого треугольника");
+            Console.WriteLine("2.Все углы этого треугольника");
+            Console.WriteLine("3.Новый треугольник,предварительно задав коэффициент подобия");
+            Console.WriteLine("Напишите запрос:Площадь,Углы,Новый");
+
+
+        }
+        public void info()
+        {
+            Console.Write("Введите сторону a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сторону b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите сторону c:");
+            c = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Проект Андрея/Проект Андрея/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Проект Андрея")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Проект Андрея")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("12e5b2a2-6952-44da-b75e-b6a4037a50a6")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
